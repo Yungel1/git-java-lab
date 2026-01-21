4cc8935873bc7b36a3ed4ded285a5d9fab21eec5
GonMa18 <demagureguisanchezgonzalo@gmail.com>
2026-01-21 17:44:35 +0100

    Add gonzalo to the participants list

--- Participants.java
+++ Participants.java
@@ -8,6 +8,7 @@ public class Participants {
         
         greet("Adrian", "Yungel1");
         greet("Mikel", "Midoiaga");
+        greet("Gonzalo", "GonMa18");
     }
 
     public static void greet(String name, String githubUser) {
