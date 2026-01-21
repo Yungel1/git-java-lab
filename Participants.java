89a2e4b34ed00d81c77cbbe0a5c9bbadb0357a56
Lucia Molinero Medel <1154072@adm.ehu.es>
2026-01-21 17:49:23 +0100

    Add [Lucía] to the participants list

--- Participants.java
+++ Participants.java
@@ -8,6 +8,7 @@ public class Participants {
         
         greet("Adrian", "Yungel1");
         greet("Mikel", "Midoiaga");
+	greet("Lucía", "LuciaMolinero")
     }
 
     public static void greet(String name, String githubUser) {
